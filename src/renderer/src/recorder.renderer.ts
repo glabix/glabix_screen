cba51133830b25e07e0b57177bf1a80abce08763
Konstantin Kim <kimkonsts@gmail.com>
2024-11-13 01:56:59 +0300

    change container to webm, use h264 codec

--- src/renderer/src/recorder.renderer.ts
+++ src/renderer/src/recorder.renderer.ts
@@ -256,7 +256,7 @@ const createVideo = (_stream, _canvas, _video) => {
       ])
 
   videoRecorder = new MediaRecorder(stream!, {
-    mimeType: "video/mp4",
+    mimeType: "video/webm;codecs=h264",
     videoBitsPerSecond: 2500000, // 2.5 Mbps
   })
 
@@ -318,7 +318,7 @@ const createVideo = (_stream, _canvas, _video) => {
       title: "videoRecorder.onstop",
     })
     timer.stop()
-    const blob = new Blob(chunks, { type: "video/webm" })
+    const blob = new Blob(chunks, { type: "video/webm;codecs=h264" })
     chunks = [] // Reset the chunks for the next recording
 
     const reader = new FileReader()
