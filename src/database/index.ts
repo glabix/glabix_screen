30706ac9c34aefd2dfa3e8c6a0eb0ab9b0c4c31c
danzel123 <mrfisher123@mail.ru>
2025-01-29 12:20:27 +0800

    try to fix win build

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,13 @@
 import { Sequelize } from "sequelize"
 import path from "path"
 import { app } from "electron"
-
+import sqlite3 from "sqlite3"
 const appDataPath = app.getPath("userData")
 const dbPath = path.join(appDataPath, "Database")
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
+  dialectModule: sqlite3,
   storage: path.join(dbPath, "db.sqlite"), // Путь к базе данных,
   logging: false,
 })
