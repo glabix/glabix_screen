b2222b0ab309e7b45b36f4eb6d61d4162f93c25e
bydiansky <bydiansky.artem@yandex.ru>
2025-01-30 08:39:31 +0300

    fix dialog level

--- src/renderer/src/dialog.renderer.ts
+++ src/renderer/src/dialog.renderer.ts
@@ -8,41 +8,39 @@ const btnTpl = document.querySelector(
   "#dialog_button_tpl"
 )! as HTMLTemplateElement
 
-document.addEventListener("DOMContentLoaded", () => {
-  window.electronAPI?.ipcRenderer.on(
-    DialogWindowEvents.RENDER,
-    (event, data: IDialogWindowData) => {
-      if (data.title) {
-        title.innerHTML = data.title
-        title.removeAttribute("hidden")
-      }
-
-      if (data.text) {
-        text.innerHTML = data.text
-        text.removeAttribute("hidden")
-      }
-
-      buttons.innerHTML = ""
-      data.buttons.forEach((btn) => {
-        const clone = btnTpl.content.cloneNode(true) as HTMLElement
-        const button = clone.querySelector("button")!
-
-        button.innerText = btn.text
-        button.classList.add(`btn--${btn.type}`)
-        button.setAttribute("data-action", btn.action)
-        buttons.appendChild(clone)
-      })
+window.electronAPI?.ipcRenderer.on(
+  DialogWindowEvents.RENDER,
+  (event, data: IDialogWindowData) => {
+    if (data.title) {
+      title.innerHTML = data.title
+      title.removeAttribute("hidden")
     }
-  )
 
-  document.addEventListener("click", (event) => {
-    const btn = event.target as HTMLButtonElement
-
-    if (btn.tagName.toLowerCase() == "button") {
-      const action = btn.dataset.action
-      window.electronAPI.ipcRenderer.send(DialogWindowEvents.CALLBACK, {
-        action,
-      })
+    if (data.text) {
+      text.innerHTML = data.text
+      text.removeAttribute("hidden")
     }
-  })
+
+    buttons.innerHTML = ""
+    data.buttons.forEach((btn) => {
+      const clone = btnTpl.content.cloneNode(true) as HTMLElement
+      const button = clone.querySelector("button")!
+
+      button.innerText = btn.text
+      button.classList.add(`btn--${btn.type}`)
+      button.setAttribute("data-action", btn.action)
+      buttons.appendChild(clone)
+    })
+  }
+)
+
+document.addEventListener("click", (event) => {
+  const btn = event.target as HTMLButtonElement
+
+  if (btn.tagName.toLowerCase() == "button") {
+    const action = btn.dataset.action
+    window.electronAPI.ipcRenderer.send(DialogWindowEvents.CALLBACK, {
+      action,
+    })
+  }
 })
