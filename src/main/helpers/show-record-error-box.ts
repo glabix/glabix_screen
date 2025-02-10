71b02c34c05c033eae91c583f1e11a21261551f0
bydiansky <bydiansky.artem@yandex.ru>
2025-02-10 15:46:40 +0300

    minor fix

--- src/main/helpers/show-record-error-box.ts
+++ src/main/helpers/show-record-error-box.ts
@@ -4,6 +4,7 @@ export const showRecordErrorBox = (header?: string, text?: string) => {
   const defaultHeader = "Критическая ошибка во время записи приложения"
   const defaultText = "Перезапустите приложение"
   ipcMain.emit("stop-recording")
+  ipcMain.emit("systemDialog:show")
   dialog.showErrorBox(header || defaultHeader, text || defaultText) // Покажет окно с ошибкой
   app.quit() // Завершает текущий процесс
 }
